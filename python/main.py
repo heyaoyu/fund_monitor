9e7e09d5e9b689a7ead4fc76dd3e29752ba94a17
heyaoyu <heyaoyu@gmail.com>
2017-12-22 16:27:03 +0800

    run offline jobs for user. reduce overlap to be continued...

--- python/main.py
+++ python/main.py
@@ -3,13 +3,13 @@ __author__ = 'heyaoyu'
 
 import tornado.ioloop
 import tornado.web
-
-from handlers.MainHandler import *
-from handlers.LongPollingHandler import *
+import tornado.gen
 
 from events.user_msgs import UserMsgs
 
 user_msgs = UserMsgs()
+from handlers.MainHandler import *
+from handlers.LongPollingHandler import *
 
 from events.events import fund_003704_monitor_job, fund_003705_monitor_job
 
@@ -26,7 +26,21 @@ def main():
     periodicCb.start()
     periodicCb2 = tornado.ioloop.PeriodicCallback(callback=fund_003705_monitor_job, callback_time=8000)  # 8000 ms
     periodicCb2.start()
+    run_job_for('user')
     tornado.ioloop.IOLoop.current().start()
+
+
+def run_job_for(user):
+    tornado.ioloop.IOLoop.add_future(tornado.ioloop.IOLoop.instance(), fund_003704_monitor_job.register(user),
+                                     future_callback)
+    tornado.ioloop.IOLoop.add_future(tornado.ioloop.IOLoop.instance(), fund_003705_monitor_job.register(user),
+                                     future_callback)
+
+
+def future_callback(future):
+    user_msgs.store_msg_for(future.user, future.result())
+    tornado.ioloop.IOLoop.add_future(tornado.ioloop.IOLoop.instance(), future.event_source.register(future.user),
+                                     future_callback)
 
 
 if __name__ == '__main__':
